c2c87a89860d56088ec97099ef89f3be1bba5ee7
Jeff Triplett <jeff.triplett@gmail.com>
2020-08-17 22:17:05 -0500

    :tractor: Ports changes

--- ajaximage/urls.py
+++ ajaximage/urls.py
@@ -1,8 +1,25 @@
-from django.urls import path
-from ajaximage import views
+try:  # pre 1.6
+    from django.conf.urls.defaults import url, patterns
+except ImportError:
+    try:
+        from django.conf.urls import url, patterns
+        urlpatterns = patterns(
+            '',
+            url(
+                '^upload/(?P<upload_to>.*)/(?P<max_width>\d+)/(?P<max_height>\d+)/(?P<crop>\d+)',
+                'ajaximage.views.ajaximage',
+                name='ajaximage'
+            ),
+        )
+    except ImportError:
+        # django 1.10
+        from django.conf.urls import url
+        from ajaximage.views import ajaximage
+        urlpatterns = [
+            url(
+                '^upload/(?P<upload_to>.*)/(?P<max_width>\d+)/(?P<max_height>\d+)/(?P<crop>\d+)',
+                ajaximage,
+                name='ajaximage'
+            ),
+        ]
 
-urlpatterns = [
-    path(r'^upload/(?P<upload_to>.*)/(?P<max_width>\d+)/(?P<max_height>\d+)/(?P<crop>\d+)',
-         views.ajaximage,
-         name='ajaximage'),
-]
